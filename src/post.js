c8fc43513e596df267373c7cf21b57e341365b46
gloriawuyl <80933162+gloriawuyl@users.noreply.github.com>
2021-11-07 16:17:38 -0800

    Create post.js with crude structural function
    The basic layout and content of Post Page are implemented, this is for users to post research information: department, title, content, and attachments.

---
+++ src/post.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+class Postform extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      department: "Engineering",
+      content: "",
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    console.log("department:"+this.state.department+"\ntitle:"+this.state.title+"\ncontent:"+this.state.content);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Create Post</h1>
+        <form onSubmit={this.handleSubmit}>
+        <div className="inputs">
+            <label>
+              Department:
+              <select name="department" onChange={this.handleInputChange}>
+                <option value="Engineering">Engineering</option>
+                <option value="Physical Science">Physical Science</option>
+                <option value="Life Science and Medical School">Life Science and Medical School</option>
+                <option value="Arts and Social Science">Arts and Social Science</option>
+              </select>
+            </label>
+          </div>
+          <br />
+          <div className="inputs">
+            <label>
+              Title:
+              <input
+                name="title"
+                type="text"
+                onChange={this.handleInputChange} />
+            </label>
+          </div>
+          <br />
+          <div className="inputs">
+            <label>
+              Content:
+              <textarea value={this.state.content} 
+              name="content" 
+              onChange={this.handleInputChange} />
+            </label>
+          </div>
+          <br />
+          <input type="file"name="attachments" className="inputs" />
+          <br />
+          <input type="submit" value="Submit" className="inputs" />
+          <input type="button" value="Cancel" className="inputs" />
+        </form>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Postform />,
+  document.getElementById('root')
+);
