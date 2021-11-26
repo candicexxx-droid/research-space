a23952579380ed881c1afe24f9af7a4537265767
Gloria Wu <gloriawuyl@g.ucla.edu>
2021-11-26 00:36:04 -0800

    connect to backend to make post and store data, but still have problems with CORS policy issues

--- src/components/MakePost.js
+++ src/components/MakePost.js
@@ -1,14 +1,15 @@
 import React from "react";
-
 import './post.css'
 
 class PostForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: "",
-      department: "Engineering",
+      Tittle: "",
+    //   department: "Engineering",
       content: "",
+      author: "eggert",
+      api_key: "eggertisgod",
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -26,8 +27,20 @@ class PostForm extends React.Component {
   }
 
   handleSubmit(event) {
-    console.log("department:"+this.state.department+"\ntitle:"+this.state.title+"\ncontent:"+this.state.content);
+    // console.log("department:"+this.state.department+"\ntitle:"+this.state.title+"\ncontent:"+this.state.content);
     event.preventDefault();
+    let url = "http://www.zyoung.tech/drivers/post.php";
+    fetch(url,{
+        method:'POST',
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        },
+        body:JSON.stringify(this.state)
+    }).then((result)=>{
+        result.json().then((res)=>{
+            console.warn('res',res)
+        })
+    })
   }
 
   render() {
