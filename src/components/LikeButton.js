254b7f5cbca819eb2b3b2fdfc4cdfd571cfbcf0b
S0u1Walker <haoran0326@g.ucla.edu>
2021-11-29 12:05:42 -0800

    Saved posts are now in Array in value1
    click on save button will check for uniqueness and append new
    id to array and update the server

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -15,19 +15,49 @@ function LikeButton(props) {
         //for word change in button
         setLiked(!liked);
         //for posting data to server
-        var params = new URLSearchParams();
-        params.append('username',Session.get("username"));
-        params.append('password',Session.get("passwd"));
-        params.append('value1', id);
-        params.append('value2', 'Saved');
-        params.append('api_key','eggertisgod');
-        axios({
-        method:'post',
-        url:'http://www.zyoung.tech/drivers/add-user.php',
-        data:params
-        }).catch(error => {
-            alert(error.message)
-        });
+        let userData = 'http://www.zyoung.tech/drivers/get-json.php?action=login&uname='+Session.get("username")+"&passwd="+Session.get("passwd");
+        //console.log(userData);
+        fetch(userData)
+            .then(response => response.json())
+            .then((jsonData) => {
+                //if the post is not in the list
+                if(!jsonData.data[0].value1){
+                    var params = new URLSearchParams();
+                    let curr = [];
+                    curr.push(id)
+                    params.append('username',Session.get("username"));
+                    params.append('password',Session.get("passwd"));
+                    params.append('value1', curr);
+                    params.append('value2', 'Saved');
+                    params.append('api_key','eggertisgod');
+                    axios({
+                    method:'post',
+                    url:'http://www.zyoung.tech/drivers/add-user.php',
+                    data:params
+                    }).catch(error => {
+                        alert(error.message)
+                    });
+
+                }else if(!jsonData.data[0].value1.split(',').includes(id)){
+                    let curr = jsonData.data[0].value1.split(',');
+                    curr.push(id);
+                    var params = new URLSearchParams();
+                    params.append('username',Session.get("username"));
+                    params.append('password',Session.get("passwd"));
+                    params.append('value1', curr);
+                    params.append('value2', 'Saved');
+                    params.append('api_key','eggertisgod');
+                    axios({
+                    method:'post',
+                    url:'http://www.zyoung.tech/drivers/add-user.php',
+                    data:params
+                    }).catch(error => {
+                        alert(error.message)
+                    });
+
+                }
+            });
+
     }
 
     return (
