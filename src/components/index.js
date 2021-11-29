e261ec1fbb8a3e140c627747dde26762dfc652b8
Yang Zhong <zhongyang88@live.com>
2021-11-29 01:48:56 +0000

    loginpage

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export { default as Navigation } from "./Navigation";
 export { default as Footer } from "./Footer";
 
-export { default as MakePost } from "./MakePost";
+export { default as MakePost } from "./Login";
 export { default as Profile } from "./profile";
 // export { default as Post } from "./home";
