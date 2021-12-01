ce1507478c64f6ec0003a542c0a8bd7528acd831
Gloria Wu <gloriawuyl@g.ucla.edu>
2021-12-01 11:36:53 -0800

    Realize the Register functionality: allowing new user to be registered, information posted & stored in backend database. After registered popup success messages and redirect user to login.

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,44 +1,81 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import axios from 'axios';
+import { Navigate } from "react-router-dom";
 import "./Login.css";
 
-export default function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  function validateForm() {
-    return username.length > 0 && password.length > 0;
+class Register extends React.Component {
+  constructor (props){
+    super(props);
+    this.state = {
+        username:"",
+        password:"",
+        value1:"",
+        value2:"",
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  function handleSubmit(event) {
+  validateForm() {
+    return this.state.username.length > 0 && this.state.password.length > 0;
+  }
+
+  handleSubmit(event) {
     event.preventDefault();
+    var params = new URLSearchParams();
+    params.append('username',this.state.username);
+    params.append('password',this.state.password);
+    params.append('Value1', "");
+    params.append('Value2', "");
+    params.append('api_key','eggertisgod');
+    axios({
+      method:'post',
+      url:'http://www.zyoung.tech/drivers/add-user.php',
+      data:params
+    })
+    .then(result => {
+      this.setState({
+          dataSent: 1,
+      })
+    })
+    .catch(error => this.setState({
+      error: error.message
+    }));
   }
 
-  return (
-    <div className="Login">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size="lg" controlId="email">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            autoFocus
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
-          Login
-        </Button>
-      </Form>
-    </div>
-  );
+  render(){
+    if(this.state.dataSent){
+      alert("Successfully Registered!");
+      return(<Navigate to='/Login'/>);
+    }
+    return (
+      <div className="Login">
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group size="lg" controlId="email">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              autoFocus
+              type="text"
+              value={this.username}
+              onChange={(e) => this.setState({username:e.target.value})}
+            />
+          </Form.Group>
+          <Form.Group size="lg" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={this.password}
+              onChange={(e) => this.setState({password:e.target.value})}
+            />
+          </Form.Group>
+          <Button block size="lg" type="submit" disabled={!this.validateForm()} className="logButton">
+            Register
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }
+
+export default Register;
